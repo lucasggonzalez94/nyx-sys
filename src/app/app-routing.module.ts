5bca92101a7d7ae6b066a447b9592a227ce99c5b
lucasggonzalez94 <lucasggonzalez94@outlook.com>
2023-10-26 12:56:05 -0300

    inicio proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    // component: AboutPageComponent
+  },
+  {
+    path: 'products',
+    // component: ContactPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
